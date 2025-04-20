4a5f78762cfc2918375e016f3a4d351cee7f5fc5
minwoohome <cmw9101@konkuk.ac.kr>
2025-04-20 14:16:34 +0900

    fix: add vanillaExtractPlugin and fix types

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [react(), dts({ insertTypesEntry: true }), vanillaExtractPlugin()],
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "ui-kit",
+      name: "ui",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
